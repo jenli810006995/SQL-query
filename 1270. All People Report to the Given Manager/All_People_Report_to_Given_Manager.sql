9e96e4582108493deaf07007f7cf48d714dcb96e
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-19 11:16:12 -0500

    Create All_People_Report_to_Given_Manager.sql

---
+++ 1270. All People Report to the Given Manager/All_People_Report_to_Given_Manager.sql
@@ -0,0 +1,11 @@
+select employee_id from employees
+where manager_id in
+(
+  select employee_id from employees where manager_id in
+  (
+    select employee_id from employees where manager_id = 1
+  )
+)
+and employee_id != manager_id;
+
+-- Link: https://leetcode.com/problems/all-people-report-to-the-given-manager/
