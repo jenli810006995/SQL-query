e84d4809403c887f3f526f0ae4b9ab7b03ed83ba
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-24 18:41:10 -0500

    Create Consecutive_Available_Seats.sql

---
+++ 603. Consecutive Available Seats/Consecutive_Available_Seats.sql
@@ -0,0 +1,8 @@
+select distinct c1.seat_id
+from cinema c1
+join cinema c2
+on abs(c1.seat_id - c2.seat_id) = 1
+and c1.free = 1 and c2.free = 1
+order by 1
+;
+-- Link: https://leetcode.com/problems/consecutive-available-seats/
